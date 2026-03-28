b8741a4831ebc30351f2d3df35e96358a5af080f
Mangrover007 <Mangrover@proton.me>
2026-03-29 03:43:06 +0530

    [impl] coded Redis consumer stream for guild messages

---
+++ internal/service/message.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"context"
+	"log"
+
+	"github.com/Mangrover007/discordv2/internal/domain"
+	"github.com/jackc/pgx/v5"
+	"github.com/redis/go-redis/v9"
+)
+
+type MessageService struct {
+	psql *pgx.Conn
+	rdb  *redis.Client
+}
+
+func NewMessageService(psql *pgx.Conn, rdb *redis.Client) *MessageService {
+	return &MessageService{
+		psql: psql,
+		rdb:  rdb,
+	}
+}
+
+func (ms *MessageService) Send_msg(msg *domain.Message) {
+	res, err := ms.psql.Query(
+		context.Background(),
+		"SELECT member_id FROM guild_user WHERE guild_id = $1",
+		msg.Guild,
+	)
+	if err != nil {
+		log.Print("ERROR: ", err.Error())
+		return
+	}
+
+	servers := make(map[string]bool)
+	for {
+		if !res.Next() {
+			break
+		}
+
+		var user_id string
+		err := res.Scan(&user_id)
+		if err != nil {
+			log.Print("ERROR: ", err.Error())
+			return
+		}
+
+		server_id, err := ms.rdb.Get(context.Background(), "user:"+user_id).Result()
+		if err != nil {
+			log.Print("ERROR: ", err.Error())
+			return
+		}
+
+		servers[server_id] = true
+	}
+
+	for server, _ := range servers {
+		ms.rdb.XAdd(context.Background(), &redis.XAddArgs{
+			Stream: server,
+			Values: map[string]interface{}{
+				"Content":  msg.Content,
+				"UserID":   msg.UserID,
+				"Guild":    msg.Guild,
+				"Channel":  msg.Channel,
+			},
+		})
+	}
+}
---
+++ internal/transport/http/router.go
@@ -0,0 +1,21 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/Mangrover007/discordv2/internal/service"
+	"github.com/jackc/pgx/v5"
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRouter(psql *pgx.Conn, rdb *redis.Client) *http.ServeMux {
+
+	ms := service.NewMessageService(psql, rdb)
+	mh := NewMessageHandler(ms)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/{guild_id}/{channel_id}", mh.Msg_handler)
+
+	return mux
+}
