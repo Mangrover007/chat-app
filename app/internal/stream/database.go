f8fac0c404bace6a3590ac30c61086a81cd1ea78
Mangrover007 <Mangrover@proton.me>
2026-04-13 01:36:14 +0530

    [feat] codebase conforms to the new schema

--- app/internal/stream/database.go
+++ app/internal/stream/database.go
@@ -4,19 +4,30 @@ import (
 	"context"
 	"log"
 	"strings"
+	"time"
 
+	"github.com/Mangrover007/discord-clone-2/app/internal/domain"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
 )
 
-func init() {
-	return
+func DB_Consumer(rdb *redis.Client, psql *pgxpool.Pool, stream_id, group_id string) {
+	// This channel is used by db_consumer to put new messages it receives from
+	// the Redis stream, and by db_worker to write them to the database.
+	db_chan := make(chan domain.DB_Msg, 100000)
+	go db_consumer(rdb, psql, stream_id, group_id, db_chan)
+	for i := 0; i < 4; i++ {
+		go db_worker(db_chan, psql, i)
+	}
 }
 
-func DB_Consumer(rdb *redis.Client, psql *pgxpool.Pool) {
-
+// NOTE: not reading from the db_chan channel directly because
+// in the future, this service will be running separately
+// AKA, in the name of scalability
+func db_consumer(rdb *redis.Client, psql *pgxpool.Pool, stream_id, group_id string, db_chan chan domain.DB_Msg) {
 	var backlog = true
-
+	var count int64 = 100000
 	for {
 		myid := ">"
 		if backlog {
@@ -26,47 +37,83 @@ func DB_Consumer(rdb *redis.Client, psql *pgxpool.Pool) {
 		res, err := rdb.XReadGroup(
 			context.Background(),
 			&redis.XReadGroupArgs{
-				Group:   "db:consumer:1",
-				Streams: []string{"db:write", myid},
-				Count:   100000,
+				Group:   group_id,
+				Streams: []string{stream_id, myid},
+				Count:   count,
 				Block:   0,
+				Consumer: "db:consumer:1",
 			},
 		).Result()
 		if err != nil {
-			if strings.Contains(strings.ToLower(err.Error()), "key") || strings.Contains(err.Error(), "no such") {
+			if strings.Contains(strings.ToLower(err.Error()), "key") {
 				continue
 			}
-			log.Print("ERROR (stream.consumer.go): ", err.Error())
+			log.Print("ERROR (stream.database.go): ", err.Error())
 			continue
 		}
-
 		if len(res[0].Messages) == 0 {
 			backlog = false
 			continue
 		}
 
 		for _, msg := range res[0].Messages {
-			_, err := psql.Exec(
-				context.Background(),
-				`INSERT INTO channel_messages (sender_id, channel_id, text_content, created_at)
-				VALUES ($1, $2, $3, $4)`,
-				msg.Values["sender_id"],
-				msg.Values["channel_id"],
-				msg.Values["text_content"],
-				msg.Values["created_at"],
-			)
-			if err != nil {
-				log.Print("ERROR (stream.consumer.go): ", err.Error())
-				log.Fatal("FATAL ERROR")
-				return
+			// log.Printf("NEW DB WRITE: %+v", msg.Values)
+			db_chan <- domain.DB_Msg{
+				Sender_id:    msg.Values["sender_id"].(string),
+				Channel_id:   msg.Values["channel_id"].(string),
+				Text_content: msg.Values["text_content"].(string),
+				Created_at:   msg.Values["created_at"].(string),
 			}
 
 			rdb.XAck(
 				context.Background(),
-				"db:writer",
-				"db:consumer:1",
+				stream_id,
+				group_id,
 				msg.ID,
 			)
+		}
+	}
+}
+
+// This function writes to the DB every 5 seconds. It is timer based instead of
+// len(db_chan) based because doing the latter would ony write to the DB when
+// the NEXT message comes in.
+func db_worker(db_chan chan domain.DB_Msg, psql *pgxpool.Pool, worker_id int) {
+	rows := make([][]interface{}, 0, 4000)
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case new_msg := <-db_chan:
+			rows = append(rows, []interface{}{
+				new_msg.Sender_id,
+				new_msg.Channel_id,
+				new_msg.Text_content,
+				new_msg.Created_at,
+			})
+
+		case <-ticker.C:
+			_, err := psql.CopyFrom(
+				context.Background(),
+				pgx.Identifier{"channel_messages"},
+				[]string{"sender_id", "channel_id", "text_content", "created_at"},
+				pgx.CopyFromRows(rows),
+			)
+			if err != nil {
+				log.Printf("ERROR (stream.database.go): %s line %d", err.Error(), 102)
+				log.Print("Trying to insert rows one by one...")
+				for _, row := range rows {
+					_, err := psql.Exec(
+						context.Background(),
+						`INSERT INTO channel_messages (sender_id, channel_id, text_content, created_at)
+						VALUES ($1, $2, $3, $4)`,
+						row[0], row[1], row[2], row[3],
+					)
+					if err != nil {
+						log.Printf("ERROR (stream.database.go): %s line %d", err.Error(), 112)
+					}
+				}
+			}
+			rows = rows[:0]
 		}
 	}
 }
--- app/internal/transport/websocket/handlers.go
+++ app/internal/transport/websocket/handlers.go
@@ -26,7 +26,6 @@ func NewHandler(wss *service.WS_Service) *WS_Handler {
 
 func (wsh *WS_Handler) UpgradeConn(w http.ResponseWriter, r *http.Request) {
 	user_id := r.Header.Get("x-uid")
-
 	log.Print("NEW CONNECTION: ", user_id)
 
 	ws, err := upgrader.Upgrade(w, r, nil)
@@ -40,37 +39,26 @@ func (wsh *WS_Handler) UpgradeConn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// ws.SetPongHandler(func(appData string) error {
-	// 	log.Print("PONG RECEIVED")
-	// 	return nil
-	// })
-
-	// ws.SetPingHandler(func(appData string) error {
-	// 	log.Print("RECEIVED PING")
-	// 	return ws.WriteMessage(websocket.PongMessage, nil)
-	// })
-
-	wc := make(chan[]byte, 2000)
+	wc := make(chan[]byte, 5000)
 	wsh.wss.Add_User(user_id, wc)
 	log.Print("WS ADDED FOR USER: ", user_id)
 
-
+	// PING-PONG setup timer.
 	timer := time.NewTicker(5 * time.Second)
 	defer timer.Stop()
+	// Reader
 	go func(){
 		for {
 			_, _, err := ws.ReadMessage()
 			if err != nil {
-				log.Print("Error (websockets.handlers.go): %s line %d", err.Error(), 64)
+				log.Printf("Error (websockets.handlers.go): %s line %d", err.Error(), 64)
 				return
 			}
 		}
 	}()
+	// Writer
 	for {
 		select {
-		// case <-wc:
-		// 	log.Print("BYE")
-		// 	return
 		case c := <-wc:
 			err := ws.WriteMessage(1, c)
 			if err != nil {
@@ -78,15 +66,11 @@ func (wsh *WS_Handler) UpgradeConn(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 		case <-timer.C:
-			// log.Print("SENDING PING: ", t)
 			err := ws.WriteMessage(websocket.PingMessage, nil)
 			if err != nil {
 				log.Print("ERROR (websockets.handlers.go): %s line %d", err.Error(), 84)
 				return
 			}
-		// default:
-		// 	log.Print("CHANNEL FULL", user_id)
-		// 	continue
 		}
 	}
 }
